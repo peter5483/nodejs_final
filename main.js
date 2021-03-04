76cb1ab6d526e0c1f989a0ff051f40fd87b46ec0
peter5483 <peter940330@gmail.com>
2021-03-04 20:45:27 +0800

    final

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
-let port = process.env.PORT || 8888
-let user_router = require('./routers/main_router.js')
+let port = process.env.PORT || 8888;
+let user_router = require('./routers/main_router.js');
 
 app.get("/", (req, res) => {
     res.send("This is main page.");
